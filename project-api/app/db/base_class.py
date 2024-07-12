038a0969c8bf6bbd18955e046626939872ad6805
nicolasjott <nicobox2002@gmail.com>
2024-07-12 13:38:27 -0400

    add created_at and update_at to Base model

--- project-api/app/db/base_class.py
+++ project-api/app/db/base_class.py
@@ -1,12 +1,24 @@
 from typing import Any
+from datetime import datetime
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy import func, TIMESTAMP
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
-
-@as_declarative()
-class Base:
+class Base(DeclarativeBase):
     id: Any
     __name__: str
+
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.CURRENT_TIMESTAMP(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.CURRENT_TIMESTAMP(),
+        onupdate=func.CURRENT_TIMESTAMP(),
+    )
+
     # Generate __tablename__ automatically
     @declared_attr
     def __tablename__(cls) -> str:
