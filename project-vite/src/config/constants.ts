27bb891744dda04384d3cbc5899ef340d952e8f9
nicolasjott <nicobox2002@gmail.com>
2024-10-03 13:35:52 -0400

    add proxy for dev server

--- project-vite/src/config/constants.ts
+++ project-vite/src/config/constants.ts
@@ -1,2 +1,4 @@
 // API
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.DEV
+    ? "/api/v1"
+    : import.meta.env.VITE_API_URL;
--- project-vite/vite.config.ts
+++ project-vite/vite.config.ts
@@ -1,24 +1,38 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    build: {
-        sourcemap: true, // Source map generation must be turned on
-    },
-    plugins: [
-        react(),
-        sentryVitePlugin({
-            org: "your-org",
-            project: "your-project",
-            authToken: process.env.SENTRY_AUTH_TOKEN,
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        build: {
+            sourcemap: true, // Source map generation must be turned on
         },
-    },
+        server: {
+            proxy: {
+                "/api/v1": {
+                    target: env.VITE_API_URL ?? "http://localhost:8000",
+                    rewrite: env.VITE_API_URL
+                        ? (path) => path.replace(/^\/api\/v1/, "")
+                        : undefined,
+                },
+            },
+        },
+        plugins: [
+            react(),
+            sentryVitePlugin({
+                org: "your-org",
+                project: "your-project",
+                authToken: env.SENTRY_AUTH_TOKEN,
+            }),
+        ],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+    };
 });
